35e5f7b57c19b1fb1fe023df7f9e56d62590468a
Junsu <sunsuking@gmail.com>
2020-12-19 21:08:44 +0900

    Add Comment(fileReader.c, returnValue.c)

--- fileReader.c
+++ fileReader.c
@@ -3,23 +3,32 @@
 
 char* openFile(FILE* ifp)
 {
+    // 만약 ifp가 NULL 이라면 파일이 없는 에러 출력
     if(ifp == NULL)
     {
         printf("File not detected!\n");
         exit(0);
     }
-    char* expr = NULL;
-    char c;
+
+    // 문자열을 저장할 str 선언 및 임시 저장할 c와 size를 초기화
+    char tmp, *str = NULL;
     int size = 0;
-    while(fscanf(ifp, "%c", &c) == 1) size++;
-    expr = (char*)malloc(sizeof(char) * (size + 1));
+
+    // 우선 처음 모든 텍스트를 확인하여 글자의 크기를 맞춘다.
+    while(fscanf(ifp, "%c", &tmp) == 1) size++;
+
+    // 글자 크기에 맞춰서 저장할 str을 동적할당함.
+    str = (char*)malloc(sizeof(char) * (size + 1));
+
+    // 파일을 다시 처음부터
     rewind(ifp);
-    // 파일 읽었던 기록 지우고 다시 처음으로.
+
+    // 문자열을 다시 읽어 이제는 str에 저장해줌.
     int i = 0;
-    while(fscanf(ifp, "%c", &c) == 1)
+    while(fscanf(ifp, "%c", &tmp) == 1)
     {
-        *(expr + i++) = c;
+        *(str + i++) = tmp;
     }
-    *(expr + i) = '\0';
-    return expr;
+    *(str + i) = '\0';
+    return str;
 }
